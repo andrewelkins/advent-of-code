3063b65cdc44471757c831335ee4812a0e20b1d3
Andrew Elkins <github@andrewelkins.com>
2019-12-20 13:56:58 -0600

    Day 4 part 2

---
+++ 2019/04/2/process.py
@@ -0,0 +1,33 @@
+# Advent of code - 2019 Day 4
+# https://adventofcode.com/2019/day/4
+
+from itertools import groupby, starmap
+from operator import le
+
+
+def has_adjacent(num):
+    return 2 in [len(list(g)) for _, g in groupby(list(str(num)))]
+    # for _, g in groupby(list(str(num))):
+    #     if len(list(g)) == 2:
+    #         return True
+
+
+def reduce_numbers(values):
+    return [x for x in values if has_adjacent(x)]
+
+
+def check_increasing(nums):
+    return all(starmap(le, zip(nums, nums[1:])))
+
+
+def process(start, stop):
+    return len(
+        [
+            x
+            for x in reduce_numbers(range(start, stop + 1))
+            if check_increasing(list(str(x)))
+        ]
+    )
+
+
+print(process(145852, 616942))
