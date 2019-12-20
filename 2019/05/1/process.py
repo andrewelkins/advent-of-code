9138ca381536603f9e4f4473d7622a1d2ff4d819
Andrew Elkins <github@andrewelkins.com>
2019-12-20 16:55:23 -0600

    Day 5 Part 1

---
+++ 2019/05/1/process.py
@@ -0,0 +1,64 @@
+# https://adventofcode.com/2019/day/2
+
+import os
+
+values = []
+with open(os.path.join(os.path.dirname(__file__), "../data.txt"), "r") as f:
+    for line in f:
+        values = list(map(int, line.strip().split(",")))
+
+
+index = 0
+while True:
+    if index >= len(values) - 1:
+        break
+
+    mode_string = str(f"{values[index]:06}")
+    
+    param1 = 0
+    param2 = 0
+    param3 = 0
+
+    try:
+        param1 = int(mode_string[-3])
+        if param1 == 0:
+            param1 = values[index + 1]
+        else:
+            param1 = index + 1
+        param2 = int(mode_string[-4])
+        if param2 == 0:
+            param2 = values[index + 2]
+        else:
+            param2 = index + 2
+        param3 = int(mode_string[-5])
+        if param3 == 0:
+            param3 = values[index + 3]
+        else:
+            param3 = index + 3
+    except IndexError:
+        pass
+
+    current_value = int(mode_string[-2:])
+    index_increment = 4
+
+    if current_value == 99:
+        break
+    elif current_value == 1:
+        values[param3] = (
+            values[param1] + values[param2]
+        )
+
+    elif current_value == 2:
+        values[param3] = (
+            values[param1] * values[param2]
+        )
+    elif current_value == 3:
+        values[param1] = int(input("Enter value: "))
+        index_increment = 2
+    elif current_value == 4:
+        print(values[param1])
+        index_increment = 2
+
+    index += index_increment
+
+print(f"Printing total: {values[0]}")
