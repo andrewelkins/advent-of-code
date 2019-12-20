d73ae801243ae0232e278d3891516953a986fdc5
Andrew Elkins <github@andrewelkins.com>
2019-12-19 18:56:46 -0600

    Add day 3

--- 2019/03/2/process.py
+++ 2019/03/2/process.py
@@ -35,6 +35,8 @@ def process(data):
     map_plot_1 = increment_xy(data[0])
     map_plot_2 = increment_xy(data[1])
     intersections = set(map_plot_1) & set(map_plot_2)
+    # much simpler answer for part 1
+    # min(abs(x) + abs(y) for (x, y) in intersections)
     # +2 for the start and end points
     return min(sum(line.index(intersect) for line in [map_plot_1, map_plot_2]) for intersect in intersections) + 2
 
